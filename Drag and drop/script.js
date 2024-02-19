2ce42e7a2b26103bb15dd8343fff16776b9accec
Jordi Sabroson <2023_jordi.manya@iticbcn.cat>
2024-02-19 18:20:39 +0100

    drag over be

--- Drag and drop/script.js
+++ Drag and drop/script.js
@@ -1,25 +1,21 @@
-array = [];
+let array = [];
 let dropArea = document.querySelector('.drop-area');
-let dragDropText = document.querySelector('h2');
-let button = document.querySelector('button');
+let dragDropText = document.querySelector('.drop-area h2');
+let button = document.querySelector('.drop-area button');
 let input = document.getElementById('input-file');
 let preview = document.getElementById('preview');
 
-dropArea.addEventListener('dragstart', dragStart);
-
-function dragStart(e) {
-    e.dataTransfer.setData('text/plain', e.target.id);
-    setTimeout(() => {
-        e.target.classList.add('hide');
-    }, 0);
-}
-
 ['dragover', 'dragleave', 'drop'].forEach(evt => {
     dropArea.addEventListener(evt, prevDefault);
-    dropArea.addEventListener('dragover', dragEnter);
+    dropArea.addEventListener('dragenter', dragEnter);
     dropArea.addEventListener('dragover', dragOver);
-    dropArea.addEventListener('dragover', dragLeave);
-    dropArea.addEventListener('dragover', drop);
+    dropArea.addEventListener('dragleave', dragLeave);
+    dropArea.addEventListener('drop', drop);
+});
+
+button.addEventListener("click", function (e) {
+    e.preventDefault();
+    input.click();
 });
 
 function prevDefault(e) {
@@ -27,17 +23,41 @@ function prevDefault(e) {
 }
 
 function dragEnter(e) {
-    e.target.classList.add('drag-over');
+    e.target.classList.add('active');
+    dragDropText.textContent = "Drop to upload files";
 }
 
 function dragOver(e) {
-    e.target.classList.add('drag-over');
+    e.target.classList.add('active');
 }
 
 function dragLeave(e) {
-    e.target.classList.remove('drag-over');
+    e.target.classList.remove('active');
+    dragDropText.textContent = "Drag & Drop files";
 }
 
 function drop(e) {
-    e.target.classList.remove('drag-over');
+    e.target.classList.remove('active');
+    dragDropText.textContent = "Drag & Drop files";
+
+    // Recollir els fitxers deixats anar
+    let fitxers = Array.from(e.dataTransfer.files)
+
+    array = array.concat(fitxers);
+
+    showFiles();
 }
+
+function showFiles() {
+    if (array.length > 0) {
+        array.forEach(element => {
+            processFile()
+        });
+    }
+}
+
+function processFile(file, index) {
+    let validExtensions = ["image/jpeg", "image/jpg", "image/png", "image/gif"];
+    let docType = file.type;
+}
+
