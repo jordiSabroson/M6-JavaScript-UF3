c020cfec7be0fe909414484a113d6d0a37b27392
Jordi Sabroson <2023_jordi.manya@iticbcn.cat>
2024-02-21 19:33:39 +0100

    pràctica acabada

--- Drag and drop/script.js
+++ Drag and drop/script.js
@@ -43,15 +43,21 @@ function drop(e) {
     // Recollir els fitxers deixats anar
     let fitxers = Array.from(e.dataTransfer.files)
 
-    array = array.concat(fitxers);
+    if (!array.includes(fitxers)) {
+        array = array.concat(fitxers);
+    } else {
+        alert("Aquest fitxer ja l'has pujat");
+    }
+    
 
     showFiles();
 }
 
 function showFiles() {
+    preview.innerHTML = "";
     if (array.length > 0) {
-        array.forEach(element => {
-            processFile()
+        array.forEach((file, index) => {
+            processFile(file, index);
         });
     }
 }
@@ -59,5 +65,46 @@ function showFiles() {
 function processFile(file, index) {
     let validExtensions = ["image/jpeg", "image/jpg", "image/png", "image/gif"];
     let docType = file.type;
+
+    // Comprovar que el fitxer tingui una extensió vàlida
+    if (!validExtensions.includes(docType)) {
+        alert("Tipus de fitxer no vàlid! Formats admesos: jpeg, jpg, png, gif");
+
+        // Si no és vàlid, es treu de l'array
+        array.splice(index, 1);
+    }
+
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onload = function () {
+        let prev = `<div class="previewImage">
+                        <img src="${reader.result}"/>
+                        <span>${file.name}</span>
+                        <span onclick="removeBtn(${index})" class="material-symbols-outlined removeBtn">c</span>
+                    </div>`;
+
+        preview.insertAdjacentHTML("beforeend", prev);
+    }
 }
 
+function removeBtn(index) {
+    array.splice(index, 1);
+
+    showFiles();
+}
+
+button.addEventListener("click", function (e) {
+    e.preventDefault();
+    input.click();
+});
+
+input.addEventListener("change", function(){
+    let selectedFiles = input.files;
+
+    array = array.concat(Array.from(selectedFiles));
+
+    showFiles();
+
+    input.value = null;
+});
