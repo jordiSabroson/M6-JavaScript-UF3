eb5756c4bef76328da31c07fa0f7d20417cf911a
Jordi Sabroson <2023_jordi.manya@iticbcn.cat>
2024-02-26 18:57:43 +0100

    acabat

--- Validació de registre/script.js
+++ Validació de registre/script.js
@@ -3,12 +3,27 @@ let nom = document.getElementById("nom");
 let email = document.getElementById("correu");
 let emailError = document.getElementById("emailError");
 let pass = document.getElementById("contrasenya");
+let check = document.getElementById("checkPass");
 let confirmPass = document.getElementById("confirmarContra");
+let errorPass = document.getElementById("errorPass");
 let postal = document.getElementById("postal");
 let submit = document.getElementById("submit");
+let valid = true;
 
-formulari.addEventListener("focusout", e => {
-    // Validar que si el camp està buit, es posa el border en vermell. Si té contingut, es posa en verd
+function validarCamp(camp) {
+    if (!camp.value) {
+        camp.style.border = '1px solid red';
+        return false;
+    } else {
+        camp.style.border = '1px solid #63b47a';
+        return true;
+    }
+}
+
+formulari.addEventListener("focusout", e => {   
+    validarCamp(e.target);
+
+    // Validar que si el camp del nom està buit, es posa el border en vermell. Si té contingut, es posa en verd
     if (!e.target.value) {
         e.target.style.border = '1px solid red';
     } else {
@@ -25,6 +40,23 @@ formulari.addEventListener("focusout", e => {
             emailError.textContent = "El correu introduït no és correcte";
         }
     }
+
+    if (valid == true) {
+        pass.style.border = '1px solid #63b47a';
+    } else if (valid == false) {
+        pass.style.border = '1px solid red';
+    }
+
+    if (comparaContra()) {
+        confirmPass.style.border = '1px solid #63b47a';
+        errorPass.textContent = '';
+    } else {
+        confirmPass.style.border = '1px solid red';
+        errorPass.textContent = 'Les contrasenyes no coincideixen'
+        errorPass.style.color = 'red';
+        errorPass.style.fontSize = '90%'
+    }
+
 });
 
 function validateEmail(email) {
@@ -35,14 +67,93 @@ function validateEmail(email) {
     }
 }
 
-pass.addEventListener("input", function() {
+pass.addEventListener("input", function () {
     let lowerCaseLetters = /[a-z]/;
     let upperCaseLetters = /[A-Z]/;
     let numbers = /[0-9]/;
     let specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    let contra = pass.value;
+    let llarg = document.getElementById("length");
+    let minus = document.getElementById("lower");
+    let majus = document.getElementById("upper");
+    let num = document.getElementById("number");
+    let esp = document.getElementById("special");
+
+    valid = true;
+
+    check.removeAttribute("hidden");
+
+    if (!(contra.length >= 8 && contra.length <= 15)) {
+        valid = false;
+        llarg.style.color = 'red';
+    } else {
+        llarg.style.color = '#63b47a';
+    }
+
+    if (!lowerCaseLetters.test(contra)) {
+        valid = false;
+        minus.style.color = 'red';
+    } else {
+        minus.style.color = '#63b47a';
+    }
+
+    if (!upperCaseLetters.test(contra)) {
+        valid = false;
+        majus.style.color = 'red';
+    } else {
+        majus.style.color = '#63b47a';
+    }
+
+    if (!numbers.test(contra)) {
+        valid = false;
+        num.style.color = 'red';
+    } else {
+        num.style.color = '#63b47a';
+    }
+
+    if (!specialChars.test(contra)) {
+        valid = false;
+        esp.style.color = 'red';
+    } else {
+        esp.style.color = '#63b47a';
+    }
+
 });
 
-// submit.addEventListener('click', e => {
-//     e.preventDefault();
-//     validarNom();
-// })
+function comparaContra() {
+    return pass.value == confirmPass.value;
+}
+
+function validarForm() {
+    let campValid = true;
+
+    if (!validarCamp(nom)) {
+        campValid = false;
+    }
+
+    if (validateEmail(email.value)) {
+        email.style.border = '1px solid #63b47a';
+        emailError.textContent = '';
+    } else {
+        email.style.border = '1px solid red';
+        emailError.textContent = "El correu introduït no és correcte";
+        campValid = false;
+    }
+
+    if (!validarCamp(pass)) {
+        campValid = false;
+        pass.style.border = '1px solid red'; 
+    }
+
+    if (!comparaContra()) {
+        campValid = false;
+    }
+
+    return campValid;
+}
+
+submit.addEventListener("click", function (e) {
+    if (!validarForm()) {
+        e.preventDefault();
+    }
+});
