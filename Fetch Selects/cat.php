9ae4796b52116d160996c1f7753f0fcbebab888f
Jordi Sabroson <2023_jordi.manya@iticbcn.cat>
2024-03-04 19:21:48 +0100

    conexio a la bbdd i retorn d'un json

--- Fetch Selects/cat.php
+++ Fetch Selects/cat.php
@@ -2,9 +2,27 @@
 include "base_de_dades.php";
 
 try{
-    $connexio = mysqli_conenct(host, database, username, pass);
+    $connexio = mysqli_connect(host, database, username, password);
     if ($connexio) {
-        
+        $cat = $_POST["cat"];
+        $query = "SELECT name FROM subcategories WHERE catId = '$cat'";
+        $subcats = mysqli_query($connexio, $query);
+
+        // Afegir els resultats de la consulta a un array
+        $return = array();
+        $object = new stdClass();
+        $object->name = $row["name"];
+        $object->subId = $row["subId"];
+
+        $return[] = $object;
+
+        echo json_encode($return);
+    } else {
+
     }
+} catch (PDOException $e) {
+    echo "Error de connexió en " . database;
+} finally {
+    mysqli_close($connect);
 }
 
---
+++ Fetch Selects/script.js
@@ -0,0 +1,7 @@
+function cambiarCategoria() {
+    fetch("cat.php")
+        .then((response) => response.json())
+        .then((data) => {
+            
+        });
+}
